b23e6657350ba49efb41e6eb27d59cc598b6f932
Vladislav Faust <vladislav.faust@gmail.com>
2015-06-02 12:11:00 +0300

    Initial commit

---
+++ core/src/shared/MyGame.java
@@ -0,0 +1,15 @@
+package shared;
+
+import com.badlogic.gdx.Game;
+
+/**
+ * Created by Faust on 02.06.2015.
+ */
+public class MyGame extends Game
+{
+	@Override
+	public void create()
+	{
+
+	}
+}
